83386581ced828748dce8b8a6fb74e0119f070e8
e <felipejuris2016@outlook.com>
2024-08-08 11:27:06 -0300

    interoperabilidade com java script

--- Source/Core/Context.cs
+++ Source/Core/Context.cs
@@ -2,14 +2,30 @@ using Microsoft.JSInterop;
 
 namespace Application.Source.Core
 {
-    public class Context(IJSRuntime js)
+    public class Context
     {
-        private readonly IJSRuntime _js = js;
-        private readonly Display _display = new(js);
+        private readonly IJSRuntime _js;
+        private readonly Display _display;
+
+        public Context(IJSRuntime js)
+        {
+            _js = js;
+            _display = new(js);
+            _js.InvokeVoidAsync(
+                "eventsInterop.initialize",
+                DotNetObjectReference.Create(this)
+            );
+        }
 
         public Display GetDisplay()
         {
             return _display;
+        }
+
+        [JSInvokable]
+        public void OnEvent(string type)
+        {
+            _js.InvokeVoidAsync("console.log", type);
         }
     }
 }
